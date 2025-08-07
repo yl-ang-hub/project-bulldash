1a5e68c6ce94c8908c0dd40949aef47da93504d9
YL Ang <skya.ayl@gmail.com>
2025-08-08 07:33:04 +0800

    Updated ui

--- src/components/ui/LoadingSpinner.jsx
+++ src/components/ui/LoadingSpinner.jsx
@@ -2,7 +2,7 @@
 import { Spinner } from "@/components/ui/shadcn-io/spinner";
 
 export const LoadingSpinner = () => (
-  <div className="mx-auto justify-self-center gap-8">
+  <div className="block mx-auto justify-self-center gap-8">
     <Spinner
       variant="infinite"
       className="h-[100px] w-[100px] text-blue-400 relative"
